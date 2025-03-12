33a9e423a335e615efcc2497ac221124a097ea08
ksy <ksy96@tigensoft.co.kr>
2025-03-12 14:55:24 +0900

    파일 업로드

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741755715',resources: [
+    cacheName: 'chirpy-1741758872',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
